38ff0bbb410c90b81e01fb0e6100ec2247954836
Ollie-Henwood <olliemhenwood2@hotmail.com>
2026-02-12 18:20:05 +0000

    Commented; cleaned

--- gimbal.ino
+++ gimbal.ino
@@ -3,15 +3,15 @@
 #include<Servo.h>
 
 Servo sg90; //SCL into A5, SDA into A4
-int servo_pin=9;
+int servo_pin = 9; //Servo connected to D9
 MPU6050 sensor;
-int16_t ax,ay,az;
-int16_t gx,gy,gz;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
 void setup(){
 sg90.attach(servo_pin);
 Wire.begin();
-Serial.begin(115200);
+Serial.begin(9600);
 Serial.println("Initializing the sensor");
 sensor.initialize();
 Serial.println(sensor.testConnection() ? "Successfully Connected":"Connection failed");
@@ -22,7 +22,7 @@ delay(1000);
 
 void loop() {
 sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-gy = map(gy,-17000,17000,0,180);
+gy = map(gy, -17000, 17000, 0, 180);
 Serial.println(gy);
 sg90.write(gy);
 delay(2);
