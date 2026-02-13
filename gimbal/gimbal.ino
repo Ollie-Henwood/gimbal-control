a60fe6d7d85e282dc738fc6ef6127d8ec2af180a
SotonQuadCopter <jhodson2022@outlook.com>
2026-02-13 14:21:49 +0000

    yawpitchroll

--- gimbal/gimbal.ino
+++ gimbal/gimbal.ino
@@ -7,11 +7,13 @@ int servo_pin = 9; //Servo connected to D9
 MPU6050 sensor;
 int16_t ax, ay, az; //accelerometer - linear acceleration
 int16_t gx, gy, gz; //gyroscope - angular acceleration
+int16_t v = 0;
+int angular_disp;
 
 void setup(){
 sg90.attach(servo_pin);
 Wire.begin();
-Serial.begin(9600);
+Serial.begin(115200);
 Serial.println("Initializing the sensor");
 sensor.initialize();
 Serial.println(sensor.testConnection() ? "Successfully Connected":"Connection failed");
@@ -22,8 +24,12 @@ delay(200);
 
 void loop() {
 sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-gy = map(gy, -17000, 17000, 0, 180);
-Serial.println(gy);
-sg90.write(gy);
+sensor.dmpGetYawPitchRoll(ypr, &q, &gravity);
+Serial.pritnln(ypr)
+gy = map(gy, -32768, 32767 , 0, 180);
+//Serial.println(gy);
+//sg90.write(angular_disp);
 delay(100);
+v = v + gy * 0.1;
+//Serial.println(gy);
 }
