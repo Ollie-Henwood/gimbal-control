c6b72ede108f17c45f4ac60e2a86882625c83860
Ollie-Henwood <olliemhenwood2@hotmail.com>
2026-02-16 15:22:30 +0000

    Maajor update

--- gimbal/gimbal.ino
+++ gimbal/gimbal.ino
@@ -7,36 +7,6 @@ Servo sg91;
 int servo_pin = 9;
 int servo_pin2 = 10;
 MPU6050 sensor;
-<<<<<<< Updated upstream
-int16_t ax, ay, az; //accelerometer - linear acceleration
-int16_t gx, gy, gz; //gyroscope - angular acceleration
-int16_t v = 0;
-int angular_disp;
-
-void setup(){
-sg90.attach(servo_pin);
-Wire.begin();
-Serial.begin(115200);
-Serial.println("Initializing the sensor");
-sensor.initialize();
-Serial.println(sensor.testConnection() ? "Successfully Connected":"Connection failed");
-delay(200);
-Serial.println("Taking Values from the sensor");
-delay(200);
-}
-
-void loop() {
-sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-sensor.dmpGetYawPitchRoll(ypr, &q, &gravity);
-Serial.pritnln(ypr)
-gy = map(gy, -32768, 32767 , 0, 180);
-//Serial.println(gy);
-//sg90.write(angular_disp);
-delay(100);
-v = v + gy * 0.1;
-//Serial.println(gy);
-}
-=======
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -89,4 +59,3 @@ void loop() {
 }
 
 
->>>>>>> Stashed changes
