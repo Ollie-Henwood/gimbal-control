69a6e741e5f45f534c13e26b42846b2523d8a8fd
SotonQuadCopter <jhodson2022@outlook.com>
2026-02-23 15:38:01 +0000

    First commit

---
+++ monitor/monitor.ino
@@ -0,0 +1,15 @@
+/*Flight mode switcher*/
+
+int pin = D1;
+unsigned long duration;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+}
+
+void loop() {
+  duration = pulseIn(pin, HIGH);
+  Serial.println(duration);
+  delay(100);
+}
