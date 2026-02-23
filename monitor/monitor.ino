3a05c9279c20bab0a0e0bdd06cd252a65d3da57c
Ollie-Henwood <olliemhenwood2@hotmail.com>
2026-02-23 17:31:13 +0000

    debugging

--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -1,6 +1,6 @@
 /*Flight mode switcher*/
 
-int mode_pin = 2;
+int mode_pin = 2; //connected to AUX1 on RX
 unsigned long duration;
 volatile byte state = LOW;
 
@@ -11,7 +11,7 @@ void setup() {
 
 void loop() {
   /*Using the interruption method*/
-  pinMode(mode_pin, INPUT_PULLUP)
+  pinMode(mode_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(mode_pin), ISR, CHANGE); //Usable pins for interrupts are 2 and 3
 }
 
