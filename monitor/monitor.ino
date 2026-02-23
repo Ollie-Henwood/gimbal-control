2516ce4c775af8d4228f52de688e9fba9c4be98b
SotonQuadCopter <jhodson2022@outlook.com>
2026-02-23 17:19:17 +0000

    Added ISR

--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -2,8 +2,10 @@
 
 int pin = 2;
 unsigned long duration;
+volatile byte state = LOW;
 
 void setup() {
+<<<<<<< Updated upstream
   Serial.begin(9600);
   pinMode(pin, INPUT);
 }
@@ -12,4 +14,30 @@ void loop() {
   duration = pulseIn(pin, HIGH);
   Serial.println(duration);
   delay(15);
+=======
+  /*Using pulseIn method*/
+  //Serial.begin(9600);
+  //pinMode(mode_pin, INPUT);
+
+  /*Using the interruption method*/
+  pinMode(mode_pin, INPUT_PULLUP)
+  attachInterrupt(digitalPinToInterrupt(mode_pin), ISR, CHANGE); //Usable pins for interrupts are 2 and 3
+}
+
+void loop() {
+  //duration = pulseIn(mode_pin, HIGH);
+  //Serial.println(duration);
+  delay(15); //because PWM frequency is 50 Hz
+}
+
+void ISR() {
+  if (state == HIGH) {
+    isLocked = True;
+  }
+
+  else () {
+    isLocked = False;
+  }
+
+>>>>>>> Stashed changes
 }
