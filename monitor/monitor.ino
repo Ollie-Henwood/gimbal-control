57e8eaa85c0a2f039b9706204a549e6afe0b7be2
Ollie-Henwood <olliemhenwood2@hotmail.com>
2026-02-26 15:06:31 +0000

    Logic

--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -3,18 +3,18 @@
 int mode_pin = 2; //connected to AUX1 on RX
 unsigned long flight_time;
 volatile byte state = LOW;
-int pulse_width;
+uint16_t pulse_width;
 
 void setup() {
   Serial.begin(9600);
   pinMode(mode_pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(mode_pin), rise, RISING);
+  attachInterrupt(digitalPinToInterrupt(mode_pin), fall, FALLING); //Usable pins for interrupts are 2 and 3
 }
 
 void loop() {
   /*Using the interruption method*/
-  pinMode(mode_pin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(mode_pin), rise, RISING);
-  attachInterrupt(digitalPinToInterrupt(mode_pin), fall, FALLING); //Usable pins for interrupts are 2 and 3
+  
 }
 
 void rise() {
@@ -22,4 +22,10 @@ void rise() {
 }
 void fall() {
   pulse_width = micros() - flight_time;
+  if (pulse_width < 2100 & pulse_width > 1600) {
+    Serial.println("HIGH");
+  }
+  else if (pulse_width < 1400 & pulse_width > 900) {
+    Serial.println("LOW");
+  }
 }
