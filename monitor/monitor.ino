1088dd69a9075ffaf4adb8b08df75c25bc604abf
SotonQuadCopter <jhodson2022@outlook.com>
2026-02-26 14:16:15 +0000

    variable name changes

--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -1,7 +1,7 @@
 /*Flight mode switcher*/
 
 int mode_pin = 2; //connected to AUX1 on RX
-unsigned long duration;
+unsigned long flight_time;
 volatile byte state = LOW;
 
 void setup() {
@@ -13,14 +13,15 @@ void loop() {
   /*Using the interruption method*/
   pinMode(mode_pin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(mode_pin), ISR, CHANGE); //Usable pins for interrupts are 2 and 3
+
 }
 
 void ISR() {
   if (state == HIGH) {
-    isLocked = True;
+    isLocked = true;
   }
 
   else () {
-    isLocked = False;
+    isLocked = false;
   }
 }
