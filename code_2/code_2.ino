cfbe7e4c8f39e7ba5d3a27d2672a71e7d4109523
SotonQuadCopter <jhodson2022@outlook.com>
2026-04-25 11:37:00 +0100

    Correcting my mistake

--- code_2/code_2.ino
+++ code_2/code_2.ino
@@ -223,7 +223,7 @@ void loop() {
     }
   }
 
-  if (done_writing == 1) && (sd.exists(flightDatName)){
+  if (done_writing == 1) && (!file.isOpen()){
     if (packet_number > 0) {
       file.write(databuffer, 256);
       file.sync();
